5b1bc52db92063dbd5b4c10fc616311b570593f1
Jagat Brahma <jpbrahma@atanford.edu>
2026-06-02 17:49:12 -0400

    Add LaTeX compile script for report
    One-command script that runs the full pdflatex + bibtex + pdflatex
    two-pass compilation and cleans up auxiliary files.

---
+++ docs/compile_report.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Recompile the CS229 final project report PDF from LaTeX source.
+# Usage: ./compile_report.sh
+
+set -e
+
+NAME="CS_229_Final_project_Report_Template"
+
+pdflatex -interaction=nonstopmode "$NAME.tex"
+bibtex "$NAME"
+pdflatex -interaction=nonstopmode "$NAME.tex"
+pdflatex -interaction=nonstopmode "$NAME.tex"
+
+# Clean up auxiliary files
+rm -f "$NAME.aux" "$NAME.log" "$NAME.out" "$NAME.bbl" "$NAME.blg"
+
+echo "Done: $NAME.pdf"
